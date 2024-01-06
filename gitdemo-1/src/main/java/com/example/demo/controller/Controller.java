8baaf4ecdba1503127a8e3e708c128eed90e33a8
ganaambhore <ganeshambhore555@gmail.com>
2024-01-07 03:21:03 +0530

    controller created

--- gitdemo-1/src/main/java/com/example/demo/controller/Controller.java
+++ gitdemo-1/src/main/java/com/example/demo/controller/Controller.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	
+	@GetMapping("/msg")
 	public String get()
 	{
-		return " ";
+		return "controller created ";
 	}
 	
 		
